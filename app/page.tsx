95d853026fb28855f18596b990941662ed9f264e
Ramandeep singh <ramandeepsingh@192.168.1.5>
2025-04-26 17:41:50 +0530

    add some change

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,38 @@
-import AboutUs from "./LandingPage/AboutUs";
-import Footer from "./LandingPage/Footer";
+// import AboutUs from "./LandingPage/AboutUs";
+// import Footer from "./LandingPage/Footer";
 // import AppDownloadSection from "./LandingPage/AppDownloadSection";
-import GallerySection from "./LandingPage/GallerySection";
-import HeroSection from "./LandingPage/HeroSection";
-import Navbar from "./LandingPage/Navbar";
-import WelcomeSection from "./LandingPage/WelcomeSection";
+// import GallerySection from "./LandingPage/GallerySection";
+// import HeroSection from "./LandingPage/HeroSection";
+// import Navbar from "./LandingPage/Navbar";
+// import WelcomeSection from "./LandingPage/WelcomeSection";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
-      {/* Navbar */}
-      <Navbar />
-      {/* HeroSection */}
-      <main className="flex-1">
-        <HeroSection />
+    // <div className="flex flex-col min-h-screen">
+    //   {/* Navbar */}
+    //   <Navbar />
+    //   {/* HeroSection */}
+    //   <main className="flex-1">
+    //     <HeroSection />
 
-        <WelcomeSection />
+    //     <WelcomeSection />
 
-        <GallerySection />
+    //     <GallerySection />
 
-        {/* <AppDownloadSection /> */}
+    //     {/* <AppDownloadSection /> */}
 
-        <AboutUs />
+    //     <AboutUs />
 
-        <Footer />
-      </main>
-    </div>
-    // <div className="flex items-center justify-center h-screen bg-cover bg-center px-4"
-    // style={{
-    //   backgroundImage: "url('https://i.postimg.cc/JnTcbvg9/bg-1.png')",
-    // }}>
-    //   <h1 className="text-3xl sm:text-5xl md:text-7xl font-bold text-gray-800 animate-pulse text-center">
-    //     🚧 Under Construction 🚧
-    //   </h1>
+    //     <Footer />
+    //   </main>
     // </div>
+    <div className="flex items-center justify-center h-screen bg-cover bg-center px-4"
+    style={{
+      backgroundImage: "url('https://i.postimg.cc/JnTcbvg9/bg-1.png')",
+    }}>
+      <h1 className="text-3xl sm:text-5xl md:text-7xl font-bold text-gray-800 animate-pulse text-center">
+        🚧 Under Construction 🚧
+      </h1>
+    </div>
   );
 }
