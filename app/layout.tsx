3aaba9f9cd9e95488490fed4549f3172e4a80c7a
Ramandeep singh <ramandeepsingh1511@gmail.com>
2025-12-09 16:19:30 +0530

    safe commit for the server

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,20 @@
 
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import { Providers } from "./Providers";
 import "react-datepicker/dist/react-datepicker.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",  
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",  
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
 
 export const metadata: Metadata = {
   title: "Salmon Arm Taxi",
@@ -32,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`antialiased`}
       >
         <Providers> {children}</Providers>
         <Toaster />
