a3f834f881b7203e4c35c8933c33d7654ca4131f
Ramandeep singh <ramandeepsingh1511@gmail.com>
2026-01-15 11:02:19 +0530

    update some design in two files

--- app/page.tsx
+++ app/page.tsx
@@ -1,49 +1,35 @@
 "use client";
-// import AboutUs from "./LandingPage/AboutUs";
-// import Footer from "./LandingPage/Footer";
+import AboutUs from "./LandingPage/AboutUs";
+import Footer from "./LandingPage/Footer";
 // // import AppDownloadSection from "./LandingPage/AppDownloadSection";
-// import GallerySection from "./LandingPage/GallerySection";
-// import HeroSection from "./LandingPage/HeroSection";
-// import Navbar from "./LandingPage/Navbar";
-// import WelcomeSection from "./LandingPage/WelcomeSection";
-// import * as motion from "motion/react-client";
-// import { useScroll } from "motion/react";
-import DashboardPage from "./admin/dashboard/page";
+import GallerySection from "./LandingPage/GallerySection";
+import HeroSection from "./LandingPage/HeroSection";
+import Navbar from "./LandingPage/Navbar";
+import WelcomeSection from "./LandingPage/WelcomeSection";
+import * as motion from "motion/react-client";
+import { useScroll } from "motion/react";
 
 export default function Home() {
-  // const { scrollYProgress } = useScroll();
+  const { scrollYProgress } = useScroll();
   return (
-    //   <motion.div style={{ scaleX: scrollYProgress }} />
-    //   {/* Navbar */}
-    //   {/* <Navbar /> */}
-    //   {/* HeroSection */}
-    //   <main className="flex-1">
-    //     <Navbar />
-    
-    //     <HeroSection />
-    
-    //     <WelcomeSection />
-
-    //     <GallerySection />
-
-    //     {/* <AppDownloadSection /> */}
-
-    //     <AboutUs />
-
-    //     <Footer />
-    //   </main>
-    // </div>
     <div className="flex flex-col min-h-screen scroll-smooth overscroll-y-contain">
-    <DashboardPage />
+       <motion.div style={{ scaleX: scrollYProgress }} />
+
+      <main className="flex-1">
+        <Navbar />
+    
+        <HeroSection />
+    
+        <WelcomeSection />
+
+        <GallerySection />
+
+        {/* <AppDownloadSection /> */}
+
+        <AboutUs />
+
+        <Footer />
+      </main>
     </div>
-    // </motion.div>
-    // <div className="flex items-center justify-center h-screen bg-cover bg-center px-4"
-    // style={{
-    //   backgroundImage: "url('https://i.postimg.cc/JnTcbvg9/bg-1.png')",
-    // }}>
-    //   <h1 className="text-3xl sm:text-5xl md:text-7xl font-bold text-gray-800 animate-pulse text-center">
-    //     🚧 Under Construction 🚧
-    //   </h1>
-    // </div>
   );
 }
